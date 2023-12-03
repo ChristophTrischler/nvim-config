10e1f6ede41acef3d67660ed7194a9da518ab0c9
CHr <christoph.trischler@t-online.de>
2023-12-03 18:40:32 +0100

    tree

--- lua/custom/plugins/tree.lua
+++ lua/custom/plugins/tree.lua
@@ -7,6 +7,9 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = "open explorer" })
+vim.keymap.set('n', '<leader>o', '<Cmd>Neotree focus<CR>', { desc = "focus explorer" })
+
 
 return {
 	"nvim-neo-tree/neo-tree.nvim",
@@ -18,23 +21,12 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		local function on_attach(bufnr)
-			local api = require "nvim-tree.api"
-
-			local function opts(desc)
-				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-			end
-
-			-- default mappings
-			api.config.mappings.default_on_attach(bufnr)
-
-			-- custom mappings
-			vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-			vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-		end
-
 		require('neo-tree').setup {
 			close_if_last_window = true,
+			window = {
+				mappings = {
+				}
+			}
 		}
 	end,
 }
