db750377b1e3f495620a82656dc870d43853506a
CHr <christoph.trischler@t-online.de>
2023-12-03 22:47:29 +0100

    tabs

---
+++ lua/custom/plugins/tabs.lua
@@ -0,0 +1,28 @@
+vim.keymap.set('n', "<tab>", "<Cmd>BufferNext<CR>")
+vim.keymap.set('n', '<S-tab>', "<Cmd>BufferPrevious<CR>")
+
+return {
+	'romgrk/barbar.nvim',
+	dependencies = {
+		'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+		'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+	},
+	opts = {
+		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+		-- animation = true,
+		-- insert_at_start = true,
+		-- …etc.
+	},
+
+	dependencies = {
+		'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+		'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+	},
+	init = function() vim.g.barbar_auto_setup = false end,
+	opts = {
+		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+		-- animation = true,
+		-- insert_at_start = true,
+		-- …etc.
+	}
+}
